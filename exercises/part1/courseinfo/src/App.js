93e762f8e4fe891419a241cb7764c6768560673e
yiiiiing <ruiyingyang0408@gmail.com>
2022-10-11 23:37:49 +0300

    finished part1.b

--- exercises/part1/courseinfo/src/App.js
+++ exercises/part1/courseinfo/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 const Header = (props) => {
+  // may have issues when use 'properties' that component receive
+  // so logging in console is a good way to debug
+  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
@@ -12,35 +15,48 @@ const Part = (props) => {
 
 const Content = (props) => {
   let items = [];
-  for (var k in props.data){
-    items.push(<Part key={k} part={k} value={props.data[k]}/>)
-  }  
+  props.data.forEach(ele => {
+    items.push(<Part key={ele.name} part={ele.name} value={ele.exercises}/>)
+  });
+
   return(
     <>{items}</>
   )
 }
 
 const Total = (props) => {
+  const total_number = props.data.reduce((total, part) => { return total + part.exercises}, 0)
   return(
     <p>
-      Number of exercises {props.data}
+      Number of exercises {total_number}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const data = {
-    'Fundamentals of React': 10,
-    'Using props to pass data': 7,
-    'State of a component': 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const total_number = Object.values(data).reduce((a, b) => a + b, 0)  
+
   return (
     <div>
-      <Header course={course} />
-      <Content data={data} />
-      <Total data={total_number} />
+      <Header course={course.name} />
+      <Content data={course.parts} />
+      <Total data={course.parts} />
     </div>
   )
 }
