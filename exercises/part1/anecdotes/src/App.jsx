7d183d8b8c2f35c5a6a39a53788a7ac9facb6a49
yiiiiing <ruiyingyang0408@gmail.com>
2024-11-22 21:23:22 +0200

    finished ex1.12-ex1.14

--- exercises/part1/anecdotes/src/App.jsx
+++ exercises/part1/anecdotes/src/App.jsx
@@ -1,34 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  // Math.random return the value between 0 and 1
+  return Math.floor(Math.random() * (max - min) + min); 
+}
 
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+const Header = ({header}) => {
+  // the name of parameters must consistent with name defined in component rendering
+  return (
+    <h1>{header}</h1>
+  )
+}
+
+const DisplayAnecdote = ({text, num_votes}) => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{text}</p>
+      <p>has {num_votes} votes</p>
     </>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const min_length = 0
+  const max_length = anecdotes.length
+  const [votes, setVotes] = useState(Array(max_length).fill(0))
+  console.log(votes)
+  const selected_most_votes = votes.indexOf(Math.max(...votes))
+  console.log(selected_most_votes)
+
+  return (
+    <div>
+      <Header header='Anecdote of the day' />
+      <DisplayAnecdote text={anecdotes[selected]} num_votes={votes[selected]} />
+      <Button text='votes' onClick={() => {
+        const votes_copy = [...votes]
+        votes_copy[selected] += 1
+        setVotes(votes_copy)
+        }} />
+      <Button text='next anecdote' onClick={() => {
+        let random_idx = getRandomInt(min_length, max_length)
+        setSelected(random_idx)
+        }} />
+      <Header header='Anecdote with the most votes'></Header>
+      <DisplayAnecdote text={anecdotes[selected_most_votes]} num_votes={votes[selected_most_votes]} />
+    </div>
   )
 }
 
