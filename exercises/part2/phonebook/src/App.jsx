e7076eca5838dce8b4963c48a4a9881bf7d259ee
yiiiiing <ruiyingyang0408@gmail.com>
2024-11-24 00:38:34 +0200

    finished 2.6

--- exercises/part2/phonebook/src/App.jsx
+++ exercises/part2/phonebook/src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Phonebook = ({persons}) => {
+  const personsComp = persons.map((p, i)=>{return <p key={i}>{p.name}</p>})
+  return personsComp
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'},
+    { name: 'Ada Lovelace'}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) =>{
+    event.preventDefault()
+    const personObj = {name: newName}
+    
+    setPersons(persons.concat(personObj))
+    setNewName('')
+  }
+  
+  const handelNameChange = (event) =>{
+    setNewName(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        name:
+        <input value={newName} onChange={handelNameChange} />
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      <Phonebook persons={persons}/>
+    </div>
   )
 }
 
