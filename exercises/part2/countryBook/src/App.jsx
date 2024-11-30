ca44c984ad398a41b1b70fa4da4b6d5cf687ae5e
yiiiiing <ruiyingyang0408@gmail.com>
2024-11-30 19:41:31 +0200

    finished 2.18

--- exercises/part2/countryBook/src/App.jsx
+++ exercises/part2/countryBook/src/App.jsx
@@ -1,11 +1,31 @@
 import { useState, useEffect } from 'react'
+import countryService from './services/country'
+import CountryForm from './components/CountryForm'
+import CountryDisplay from './components/CountryDisplay'
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState("")
+
+  const onFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  useEffect(() => {
+    if (filter) {
+      console.log('fetching countries...')
+      countryService
+      .getAllByFilter(filter)
+      .then((data) => {
+        setCountries(data)
+      })
+    }
+  }, [filter])
 
   return (
     <div>
-      <h2>Find Country</h2>
+      <CountryForm countryFilter={filter} onFilterChange={onFilterChange}/>
+      <CountryDisplay countries={countries}/>
     </div>
   )
 }
